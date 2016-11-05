d9fb84146dfb632f6fa96d5a02ac818328bcd733
AndreasN <andexander@hotmail.com>
2016-11-05 19:52:31 +0200

    initclock.c kustutatud ebavajalikud read

--- src/initClock.c
+++ src/initClock.c
@@ -1,20 +1,6 @@
-#include <msp430.h>
-
-
 #include "initClock.h"
 #include "MSP430F5xx_6xx/driverlib.h"
 
-int main(void)
-{
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-  P1DIR |= 0x01;                            // P1.0 output
-  TA0CCTL0 = CCIE;                          // CCR0 interrupt enabled
-  TA0CCR0 = 65000;
-  TA0CTL = TASSEL_2 + MC_1 + TACLR;         // SMCLK, upmode, clear TAR
-
-  __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, enable interrupts
-  __no_operation();                         // For debugger
-}
 
 
 
@@ -37,16 +23,4 @@ int set_clock(){
 
 	UCS_initFLLSettle(MCLK_FREQ_KHZ, MCLK_FLLREFERENCE_RATIO);
 	return 0;
-
-// Timer0 A0 interrupt service routine
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void TIMER0_A0_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) TIMER0_A0_ISR (void)
-#else
-#error Compiler not supported!
-#endif
-{
-  P1OUT ^= 0x01;                            // Toggle P1.0
 }
