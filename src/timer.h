c1b381fab6dc6d017c0416b042d5a4cd3da62968
Madis Vahtrik <madisvahtrik@gmail.com>
2016-11-07 14:33:09 +0200

    timer.h comments

--- src/timer.h
+++ src/timer.h
@@ -7,7 +7,7 @@
 
 #ifndef TIMER_H_
 #define TIMER_H_
-#define ClockCyclesCount 32000 /*see on uint16, mis l‰heb compare registrisse CCR0 ning timer_A countib selle arvuni, olenevalt clock kiirusest saab selle arvuga m‰‰rata, nt interrupt intervalle*/
+//#define ClockCyclesCount 32000 /*see on uint16, mis l‰heb compare registrisse CCR0 ning timer_A countib selle arvuni, olenevalt clock kiirusest saab selle arvuga m‰‰rata, nt interrupt intervalle*/
 
 /* ainult enda kirjutatud funcid ning mida saab v‰ljast kutsuda; muud timer.c failis olevad funcid on olemas "MSP430F5xx_6xx/driverlib.h"*/
 void timer_init();
